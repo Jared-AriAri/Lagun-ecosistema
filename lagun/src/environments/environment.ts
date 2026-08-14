5d5025c90a5d9dc4dd810cba37008648921849c2
Jared Ari <jaredariel5193@gmail.com>
2026-08-14 11:41:36 -0600

    Correcciones de la Evaluacion 2

--- lagun/src/environments/environment.ts
+++ lagun/src/environments/environment.ts
@@ -2,6 +2,6 @@ import type { Environment } from './environment.prod';
 
 export const environment: Environment = {
   production: false,
-  supabaseUrl: 'https://phfcxfegzwjzwzqacjgy.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZmN4ZmVnendqend6cWFjamd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNzc1NDksImV4cCI6MjA4Njk1MzU0OX0.N0_tfl_GQGE_pFGu43DzkkxRL7ZnZd8LOYrAP7YyJ3k'
+  supabaseUrl: 'Tu_url',
+  supabaseAnonKey: 'Tu_anonkey'
 };
